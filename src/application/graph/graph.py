c1fa01fbdd5d64745d816347d3403697ac6e153f
Eduardo Lima <eduefi@gmail.com>
2026-05-05 22:40:12 +0200

    add logic to iterate over subquestions

--- src/application/graph/graph.py
+++ src/application/graph/graph.py
@@ -14,6 +14,7 @@ from src.application.ports.outbound.model_client_port import ModelClientPort
 def blocked(_):
     return {"messages": [AIMessage("Apologies but a security risk was detected in your last prompt.")]}
 
+
 def initial_checks_condition(state: dict):
     blocked = state["safeguard"].blocked if state.get("safeguard") else False
 
@@ -21,6 +22,21 @@ def initial_checks_condition(state: dict):
         return "unsafe"
 
     return "safe"
+
+
+def iteration_condition(state: dict):
+    error = state["error"] if "error" in state else None
+    current_step = state["current_step"] if "current_step" in state else None
+    total_steps = state["total_steps"] if "total_steps" in state else None
+
+    if error or not current_step or not total_steps:
+        return "done"
+
+    # current step is an index whereas total_steps is a count. If they are equal, it means it reached the last index
+    if current_step == total_steps:
+        return "done"
+
+    return "iterate"
 
 
 def get_graph_definition(model_client: ModelClientPort, memory_saver: MemoryPort):
@@ -44,7 +60,14 @@ def get_graph_definition(model_client: ModelClientPort, memory_saver: MemoryPort
 
     # after initial checks
     agent_builder.add_edge("plan_query", "cypher_generator")
-    agent_builder.add_edge("cypher_generator", "summarize")
+    agent_builder.add_conditional_edges(
+        "cypher_generator",
+        iteration_condition,
+        {
+            "iterate": "cypher_generator",
+            "done": "summarize"
+        },
+    )
     agent_builder.add_edge("blocked", "summarize")
     agent_builder.add_edge("summarize", END)
 
