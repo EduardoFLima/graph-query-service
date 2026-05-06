511f2468f36322f1c7c436e917557496ff7c1e89
Eduardo Lima <eduefi@gmail.com>
2026-05-06 23:35:33 +0200

    make neo4j repository to return converted neo4j types
    in order to make sure they are serializable

--- src/adapters/outbound/persistence/neo4j_purchase_repository.py
+++ src/adapters/outbound/persistence/neo4j_purchase_repository.py
@@ -1,4 +1,5 @@
 from neo4j import GraphDatabase, exceptions
+from neo4j.time import DateTime, Date, Time
 
 from src.application.ports.outbound.purchase_repository import PurchaseRepository
 
@@ -8,10 +9,24 @@ class Neo4jPurchaseRepository(PurchaseRepository):
     def __init__(self, uri: str, user: str, password: str):
         self._driver = GraphDatabase.driver(uri, auth=(user, password))
 
+    @staticmethod
+    def _convert_neo4j_types(value):
+        if isinstance(value, DateTime):
+            return value.iso_format()
+        if isinstance(value, Date):
+            return value.iso_format()
+        if isinstance(value, Time):
+            return value.iso_format()
+        if isinstance(value, dict):
+            return {k: Neo4jPurchaseRepository._convert_neo4j_types(v) for k, v in value.items()}
+        if isinstance(value, list):
+            return [Neo4jPurchaseRepository._convert_neo4j_types(item) for item in value]
+        return value
+
     def execute_cypher(self, cypher: str) -> list[dict]:
         with self._driver.session() as session:
             result = session.run(cypher)
-            return [record.data() for record in result]
+            return [self._convert_neo4j_types(record.data()) for record in result]
 
     def validate_cypher(self, cypher: str) -> dict:
         try:
