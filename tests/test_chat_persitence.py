08467f824e31e7ad5805eff712d04768e035aa37
Eduardo Lima <eduefi@gmail.com>
2026-05-11 21:59:33 +0200

    add checkpoint tests

--- tests/test_chat_persitence.py
+++ tests/test_chat_persitence.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 
@@ -11,7 +13,31 @@ class TestChatPersistence:
         return TestClient(app)
 
     def test_graph_should_remember_previous_iteration(self, client):
-        pass
+        thread_id = "test_graph_should_remember_previous_path" + str(datetime.now())
+        # first call
+        client.cookies.set("thread_id", thread_id)
+        first_question = "list the products sold last week."
+        response = client.post("/chat", json={"prompt": first_question})
+        assert response.status_code == 200
+
+        # the second call should remember the path
+        response = client.post("/chat?show_history=true", json={"prompt": "What was my last question?"})
+        assert response.status_code == 200
+        assert response.json() is not None
+        messages = response.json().get("messages")
+
+        assert first_question in messages[0]
 
     def test_graph_should_keep_memory_when_thread_id_is_not_given(self, client):
-        pass
+        # first call
+        first_question = "list the products sold last week."
+        response = client.post("/chat", json={"prompt": first_question})
+        assert response.status_code == 200
+
+        # the second call should remember the path
+        response = client.post("/chat?show_history=true", json={"prompt": "What was my last question?"})
+        assert response.status_code == 200
+        assert response.json() is not None
+        messages = response.json().get("messages")
+
+        assert first_question in messages[0]
